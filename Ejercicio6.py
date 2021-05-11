f6c268d76ef30533260e54ed1592416a392fd337
VIVIANA ANDREA ANDREA <>
2021-05-11 03:51:36 +0000

    Ejercicio 6

---
+++ Ejercicio6.py
@@ -0,0 +1,15 @@
+archivo = open('paises.txt', 'r')
+#Cuente e imprima cuantas capitales inician con la latra M
+lista=[]
+ciudad=[]
+for i in archivo:
+  a=i.index(":")
+  for r in range(a+2,len(i)):
+    lista.append(i[r])
+  a="".join(lista)
+  ciudad.append(a)
+  lista=[]
+for i in ciudad:
+  if(i[0]=="M"):
+    print(i)
+archivo.close()
